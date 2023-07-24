9f90ca0d42a84cd63dd4ee0ea6ed71042e3cfbb9
explosiver1 <jrdholston@gmail.com>
2023-07-24 00:35:26 -0500

    avalonia remake of project from wpf

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PhoenixCalculator_Avallon.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
